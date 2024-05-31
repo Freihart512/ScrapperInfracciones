9d2a1330409802a5883488e87ee0f06085868566
Carlos Gonzalez <carlosfabian512@gmail.com>
2024-05-31 12:00:34 -0600

    pr1

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
 export function greet(name: string) {
   return `Hello, ${name}!`;
 }
-if (greet('hola') === 'hola') {
-  console.log('hey d');
-}
-
-console.log(
-  greet('jhfjhfjfworld holaaafggffiudgsdgghjjihgkjhjvgjhgjhhkjhkjdddaa'),
-);
